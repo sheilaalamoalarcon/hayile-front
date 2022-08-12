f24b7cb0543d8cb1b9bb953b420ccfa665b24711
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 18:37:39 +0100

    moved to src folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); // create a router.get method to resolve index.html
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/src/index.html'));
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('Running.'))
