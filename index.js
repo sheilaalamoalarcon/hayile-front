698ceff42ca4ff95b447dfb7b18f43774e8cc705
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 18:17:45 +0100

    dirname added

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const router = Router();
 
 router.get('/', function (req, res) {
-    res.sendFile('./index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 router.get('/index.html', function (req, res) {
-    res.sendFile('./index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use('/', router);
 app.listen(process.env.PORT || 3000, () => console.log('Running.'))
+
+
