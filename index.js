f1e474e7a9897caa3c4f1f99aa0ae2e42995d8ff
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 18:06:54 +0100

    9k+2 th try

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const router = Router();
 
 router.get('/', function (req, res) {
-    res.sendFile('./src/index.html');
+    res.sendFile('src/index.html');
 });
 
 router.get('/index.html', function (req, res) {
-    res.sendFile('./src/index.html');
+    res.sendFile('src/index.html');
 });
 
 app.use('/', router);
