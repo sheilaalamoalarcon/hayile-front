0396678feec77c998c37a3401309eb3a5c3c195c
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 17:47:45 +0100

    start of succefully deploy

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express()
 
 app.get('/', function (req, res) {
-    res.sendFile('./src/index.html');
+    res.sendFile('src/index.html');
 });
 app.listen(process.env.PORT || 5000), () => console.log('Server working.')
