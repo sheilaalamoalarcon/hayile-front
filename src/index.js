4d97064ff65360f21ecfae596d3b595dec64a3e3
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 17:09:40 +0100

    Code Cleanup and port fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express');
 
-const port = process.env.PORT || 3000
+const app = express()
 
-http.createServer((req, res) => {
-    const filePath = __dirname + (req.url === '/' ? '/index.html' : req.url)
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(404)
-            res.end(JSON.stringify(err))
-            return
-        }
-        res.writeHead(200)
-        res.end(data)
-    })
-}).listen(port)
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(3000, () => console.log('Server working.'))
