c881aa493566b1aece7b13c60464317f41565e8b
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 18:04:43 +0100

    9k th try

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express, { Router } from 'express';
 const app = express();
-import { join } from 'path';
 const router = Router();
 
 router.get('/', function (req, res) {
-    res.sendFile(join(__dirname + '/src/index.html'));
+    res.sendFile('/src/index.html');
 });
 
-
-
+router.get('/index.html', function (req, res) {
+    res.sendFile('/src/index.html');
+});
 
 app.use('/', router);
 app.listen(process.env.PORT || 3000, () => console.log('Running.'))
