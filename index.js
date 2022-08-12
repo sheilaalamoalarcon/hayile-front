33e9c7ad85a89b8f2628445921f9d776655ead9c
MaximoDM <rtp.iea2010@gmail.com>
2022-08-12 20:53:13 +0100

    sending css at app get

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express(); // create a router.get method to resolve index.html 
 const path = require('path');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/index.html'));
 })
+
+app.get('/src/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/index.css'));
+})
+
 
 app.listen(process.env.PORT || 3000, () => console.log('Running.'))
 
