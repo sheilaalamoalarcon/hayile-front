fcff16ea2fdd24bcdff6e907b9315cb21a907b05
MaximoDM <rtp.iea2010@gmail.com>
2022-08-13 10:55:37 +0100

    css need to specify the root

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express(); // create a router.get method to resolve index.html 
+const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
