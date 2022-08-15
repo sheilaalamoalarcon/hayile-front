6f7be5356ef4b7862da1e5f5471b85c24f80b2a1
“Alexis <alexisplettener@gmail.com>
2022-08-15 12:16:58 +0100

    index.html: renombrar clases y id: (.shortcuts__list -> .shortcuts__file)(.myUL -> .file__section)(#myInput -> #input-search).index.css: ajustar los nombres de las clases e id. main.js: Nueva funcionalidad: hideEmpySections() -> esconde los ul cuando no hay ningún shortcut que coincida con la búsqueda.Refactor y renaming en la funcionalidad de busqueda del input.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
 /* eslint-disable indent */
-function myFunction() {
-    let input, filter, ul, li, p, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toLowerCase();
+const inputSearcher = document.getElementById("input-search");
+const ulSections = document.getElementsByClassName("file__section");
 
-    ul = document.getElementsByClassName("myUl");
-    for (i = 0; i < ul.length; i++) {
-        li = ul[i].getElementsByTagName("li");
+const searchShortcuts = () => {
+    const typedWords = inputSearcher.value.toLowerCase();
+    for (let i = 0; i < ulSections.length; i++) {
+        const ulSection = ulSections[i];
+        const li = ulSection.getElementsByTagName("li");
         for (let j = 0; j < li.length; j++) {
-            p = li[j].getElementsByTagName("p")[0];
-            if (p.innerHTML.toLowerCase().indexOf(filter) > -1) {
+            const p = li[j].getElementsByTagName("p")[0];
+            if (p.innerHTML.toLowerCase().indexOf(typedWords) > -1) {
                 li[j].style.display = "";
             } else {
                 li[j].style.display = "none";
             }
         }
     }
-}
+    hideEmptySections();
+};
+const hideEmptySections = () => {
+    for (let i = 0; i < ulSections.length; i++) {
+        const ulSection = ulSections[i];
+        const li = ulSection.getElementsByTagName("li");
+        let count = 0;
+        for (let j = 0; j < li.length; j++) {
+            if (li[j].style.display === "none") {
+                count++;
+            }
+        }
+        if (count === li.length) {
+            ulSection.style.display = "none";
+        } else {
+            ulSection.style.display = "";
+        }
+    }
+};
+inputSearcher.addEventListener("keyup", searchShortcuts);
